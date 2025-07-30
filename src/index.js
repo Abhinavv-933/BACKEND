075ee38029539faeb952170b383a41814fba2109
Abhinav Dwivedi <abhinavdwivedi933@gmail.com>
2025-07-30 17:00:10 +0530

    Connect to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path: '.env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+  
+
+
+// database is jab v baat kro try catch use kro aur async await ka use kro
+// semi-colon used before iffies are for cleaning purpose 
+
+/*
+import express from "express"
+const app = express()
+
+( async() => {  //iffy concept from javascript
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("ERROR", (error) => {
+      console.log("Error", error);
+      throw error;
+    })
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+
+  } catch (error) {
+    console.log("ERROR", error)
+  }
+})
+  */
